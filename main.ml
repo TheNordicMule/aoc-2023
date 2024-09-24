7a33297a12c3321ff5876d34b85412947e637b3e
Mingshi Wang <mingshiwang123@gmail.com>
2024-09-24 10:08:48 -0500

    init project

--- main.ml
+++ main.ml
@@ -1,19 +1,19 @@
 open Core
 
-let match_string_with_digit = function
-  | "one" -> Some 1
-  | "two" -> Some 2
-  | "three" -> Some 3
-  | "four" -> Some 4
-  | "five" -> Some 5
-  | "six" -> Some 6
-  | "seven" -> Some 7
-  | "eight" -> Some 8
-  | "nine" -> Some 9
-  | _ -> None
-
-let lst_of_digit_string =
-  [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
+(*let match_string_with_digit = function*)
+(*  | "one" -> Some 1*)
+(*  | "two" -> Some 2*)
+(*  | "three" -> Some 3*)
+(*  | "four" -> Some 4*)
+(*  | "five" -> Some 5*)
+(*  | "six" -> Some 6*)
+(*  | "seven" -> Some 7*)
+(*  | "eight" -> Some 8*)
+(*  | "nine" -> Some 9*)
+(*  | _ -> None*)
+(**)
+(*let lst_of_digit_string =*)
+(*  [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]*)
 
 let solution =
   (* Function to read the file lines *)
