103099f75f039bead9e9b5952eb5cc27d13a32ec
Mingshi Wang <mingshiwang123@gmail.com>
2024-09-25 23:23:26 -0500

    pass day 1

--- bin/day2.ml
+++ bin/day2.ml
@@ -11,7 +11,6 @@ let judge_number_is_not_sufficient couple =
 ;;
 
 let calculate_line_should_score line =
-  (*let _ = Fmt.pr "@.line: %s@." line in*)
   let parts = String.split ~on:':' line in
   let game = List.nth_exn parts 1 in
   let turns = String.split ~on:';' game in
@@ -23,22 +22,14 @@ let calculate_line_should_score line =
   | None -> true
 ;;
 
-let rec calculate_game_number lst =
-  match lst with
-  | _ :: 'm' :: 'e' :: ' ' :: a :: ':' :: _ ->
-    let number = Fmt.str "%s" (String.of_char a) in
-    number
-  | _ :: 'm' :: 'e' :: ' ' :: a :: b :: _ ->
-    let number = Fmt.str "%s" (String.of_char_list [ a; b ]) in
-    number
-  | _ :: t -> calculate_game_number t
-  | _ -> failwith "error parsing problem"
+let calculate_game_number line =
+  let parts = List.hd_exn (String.split ~on:':' line) in
+  List.nth_exn (parts |> String.strip |> String.split ~on:' ') 1 |> int_of_string
 ;;
 
 let calculate_line_score acc line =
-  let lst = line |> String.to_list in
-  let game_number = int_of_string (calculate_game_number lst) in
-  let _ = calculate_line_should_score line |> Fmt.pr "@.line_should_score: %b@." in
+  let game_number = calculate_game_number line in
+  (*let _ = game_number |> Fmt.pr "@.line_should_score: %i@." in*)
   match calculate_line_should_score line with
   | true -> acc + game_number
   | false -> acc
