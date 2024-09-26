1f055d67c56ef721204d14fe150e97beb89b6b3a
Mingshi Wang <mingshiwang123@gmail.com>
2024-09-25 23:07:24 -0500

    add logic(need debug)

--- bin/day2.ml
+++ bin/day2.ml
@@ -1,13 +1,27 @@
 open Core
 
-(*type color =*)
-(*  | Red of int*)
-(*  | Green of int*)
-(*  | Blue of int*)
+let judge_number_is_not_sufficient couple =
+  match String.split ~on:' ' (couple |> String.strip) with
+  | [ number; "red" ] -> int_of_string number > 12
+  | [ number; "green" ] -> int_of_string number > 13
+  | [ number; "blue" ] -> int_of_string number > 14
+  | any ->
+    Fmt.pr "@.Unmatched parts: %a@." (Fmt.list Fmt.string) any;
+    failwith "Pattern matching failed - input not as expected"
+;;
 
-(*let substr line index = String.sub ~pos:index ~len:(String.length line - index) line*)
-
-let calculate_line_should_score _line = false
+let calculate_line_should_score line =
+  (*let _ = Fmt.pr "@.line: %s@." line in*)
+  let parts = String.split ~on:':' line in
+  let game = List.nth_exn parts 1 in
+  let turns = String.split ~on:';' game in
+  let details = List.map turns ~f:(fun a -> String.split ~on:',' a) |> List.concat in
+  (*Fmt.pr "details: @.%a" (Fmt.list Fmt.string) details;*)
+  let break = List.find details ~f:judge_number_is_not_sufficient in
+  match break with
+  | Some _ -> false
+  | None -> true
+;;
 
 let rec calculate_game_number lst =
   match lst with
@@ -23,8 +37,8 @@ let rec calculate_game_number lst =
 
 let calculate_line_score acc line =
   let lst = line |> String.to_list in
-  let _ = calculate_game_number lst |> Fmt.pr "@.parsed: %s@." in
   let game_number = int_of_string (calculate_game_number lst) in
+  let _ = calculate_line_should_score line |> Fmt.pr "@.line_should_score: %b@." in
   match calculate_line_should_score line with
   | true -> acc + game_number
   | false -> acc
@@ -32,7 +46,7 @@ let calculate_line_score acc line =
 
 let pt_1 =
   let r file = In_channel.read_lines file in
-  let content = r "./input/day2.txt" in
+  let content = r "./input/day2-prod.txt" in
   let answer = List.fold content ~init:0 ~f:calculate_line_score in
   answer
 ;;
