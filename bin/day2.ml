1751076297801387d75d1374c355d405ea266a37
Mingshi Wang <mingshiwang123@gmail.com>
2024-09-26 19:33:15 -0500

    finish day2

--- bin/day2.ml
+++ bin/day2.ml
@@ -42,4 +42,47 @@ let pt_1 =
   answer
 ;;
 
-let _ = pt_1 |> Fmt.pr "@.part 1: %i"
+(*separator part 1 and 2*)
+
+type game_color =
+  { red : int
+  ; green : int
+  ; blue : int
+  }
+
+let calculate_line_color acc curr =
+  match curr |> String.strip |> String.split ~on:' ' with
+  | [ number; "red" ] when int_of_string number > acc.red ->
+    { red = int_of_string number; green = acc.green; blue = acc.blue }
+  | [ number; "green" ] when int_of_string number > acc.green ->
+    { red = acc.red; green = int_of_string number; blue = acc.blue }
+  | [ number; "blue" ] when int_of_string number > acc.blue ->
+    { red = acc.red; green = acc.green; blue = int_of_string number }
+  | _ -> acc
+;;
+
+let calculate_line_power = function
+  | { red; green; blue } -> red * green * blue
+;;
+
+let calculate_game_power_score acc line =
+  let parts = String.split ~on:':' line in
+  let game = List.nth_exn parts 1 in
+  let turns = String.split ~on:';' game in
+  let details = List.map turns ~f:(fun a -> String.split ~on:',' a) |> List.concat in
+  let line_game_color =
+    List.fold details ~init:{ red = 0; green = 0; blue = 0 } ~f:calculate_line_color
+  in
+  let line_power = calculate_line_power line_game_color in
+  line_power + acc
+;;
+
+let pt_2 =
+  let r file = In_channel.read_lines file in
+  let content = r "./input/day2-prod.txt" in
+  let answer = List.fold content ~init:0 ~f:calculate_game_power_score in
+  answer
+;;
+
+let _ = pt_1 |> Fmt.pr "@.part 1: %i" in
+pt_2 |> Fmt.pr "@.part2: %i"
